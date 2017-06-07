2c3510ab520a623ab364f9d407b714f6a841b50d
Colin Fendrick <colin.fendrick@gmail.com>
2017-06-07 10:00:07 -0400

    Updating API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
-const API_KEY ='f6a4d558de9047bc8d89907960834b60'
+const API_KEY ='f6a4d558de9047bc8d89907960834b60';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return{
+    type: FETCH_WEATHER
+  };
+}
