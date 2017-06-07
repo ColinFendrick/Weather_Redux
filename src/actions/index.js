5d3e36bad10cd996b71fe150711042d4e6e5b4e6
Colin Fendrick <colin.fendrick@gmail.com>
2017-06-07 10:20:12 -0400

    Updated API url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 const API_KEY ='f6a4d558de9047bc8d89907960834b60';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
-  return{
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+
+  return {
     type: FETCH_WEATHER
   };
 }
+`
